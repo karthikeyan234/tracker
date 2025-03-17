a961c4b6857fd12ff4fa06aab32a683688a9c582
karthikeyan_paramasivan <karthikeyan_paramasivan@medilink.ph>
2025-03-17 19:03:37 +0530

    Initial Commit

---
+++ lib/expense_tracker/application.ex
@@ -0,0 +1,36 @@
+defmodule ExpenseTracker.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      ExpenseTrackerWeb.Telemetry,
+      ExpenseTracker.Repo,
+      {DNSCluster, query: Application.get_env(:expense_tracker, :dns_cluster_query) || :ignore},
+      {Phoenix.PubSub, name: ExpenseTracker.PubSub},
+      # Start the Finch HTTP client for sending emails
+      {Finch, name: ExpenseTracker.Finch},
+      # Start a worker by calling: ExpenseTracker.Worker.start_link(arg)
+      # {ExpenseTracker.Worker, arg},
+      # Start to serve requests, typically the last entry
+      ExpenseTrackerWeb.Endpoint
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: ExpenseTracker.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  @impl true
+  def config_change(changed, _new, removed) do
+    ExpenseTrackerWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
